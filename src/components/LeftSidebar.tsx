db7225bdc850bd3cfece5ace295e3b650abe4609
Andy Brenneke <andy.brenneke@ironcladhq.com>
2023-04-22 19:12:44 -0700

    Toggle side bar
    Former-commit-id: becfb01afd46ad1b54b6cf9a2d8d8af4baa45922 [formerly 61a57fdcabebd150241784d9150d8caa4a1c8aec [formerly fb73147d1be2658eb0a4504dd569e69cdde50d16]]
    Former-commit-id: 61a57fdcabebd150241784d9150d8caa4a1c8aec
    Former-commit-id: bfcef73cd023d93afbedb3ea5ed36bee712f283c

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -11,6 +11,8 @@ import { useSaveCurrentGraph } from '../hooks/useSaveCurrentGraph';
 import DropdownMenu, { DropdownItem } from '@atlaskit/dropdown-menu';
 import Button from '@atlaskit/button';
 import { ReactComponent as MoreIcon } from 'majesticons/line/more-menu-vertical-line.svg';
+import { ReactComponent as ExpandLeftIcon } from 'majesticons/line/menu-expand-left-line.svg';
+import { ReactComponent as ExpandRightIcon } from 'majesticons/line/menu-expand-right-line.svg';
 
 const styles = css`
   position: fixed;
@@ -20,7 +22,6 @@ const styles = css`
   width: 300px; // Adjust the width of the sidebar as needed
   background-color: var(--grey-dark);
   padding: 1rem;
-  overflow: hidden;
   z-index: 50;
   border-right: 1px solid var(--grey);
 
@@ -54,6 +55,28 @@ const styles = css`
     padding: 4px 8px;
     flex: 1;
   }
+
+  .toggle-tab {
+    position: absolute;
+    top: 10px;
+    right: -32px;
+    background-color: var(--grey-dark);
+    border: 1px solid var(--grey);
+    border-left: 0;
+    border-radius: 0 8px 8px 0;
+    width: 32px;
+    height: 32px;
+    font-size: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    z-index: 100;
+  }
+
+  .toggle-tab:hover {
+    background-color: var(--grey-darkish);
+  }
 `;
 
 const moreDropdownCss = css`
@@ -66,6 +89,7 @@ export const LeftSidebar: FC = () => {
   const [graph, setGraph] = useRecoilState(graphState);
   const [savedGraphs, setSavedGraphs] = useRecoilState(savedGraphsState);
   const saveCurrentGraph = useSaveCurrentGraph();
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
 
   const sortedGraphs = orderBy(savedGraphs, ['metadata.name'], ['asc']);
 
@@ -86,7 +110,13 @@ export const LeftSidebar: FC = () => {
   }
 
   return (
-    <div css={styles}>
+    <div
+      css={styles}
+      style={{ transform: isSidebarVisible ? 'translateX(0)' : 'translateX(-100%)', transition: 'transform 0.3s ease' }}
+    >
+      <div className="toggle-tab" onClick={() => setIsSidebarVisible(!isSidebarVisible)}>
+        {isSidebarVisible ? <ExpandLeftIcon /> : <ExpandRightIcon />}
+      </div>
       <div className="graph-info-section">
         <label>Graph Name</label>
         <InlineEditableTextArea
