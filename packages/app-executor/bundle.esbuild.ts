9c5d8a149dc5cb27895722ce36ec6d5f717fa4f3
Andy Brenneke <andy.brenneke@ironcladhq.com>
2023-06-11 21:20:11 -0700

    Raise events externally
    Former-commit-id: 95e0a2658f4e29fbae3aa1abed40d0c2ef3f1ea6 [formerly 6afa667511c3db9fa4dbd70fc9088826db5923ce [formerly 93cdb31c534c555c60696c9271d7cbe0cf43d4a1]]
    Former-commit-id: 6afa667511c3db9fa4dbd70fc9088826db5923ce
    Former-commit-id: e81480fb9a071fc499507244bfaf4c3f290b7eb9

--- packages/app-executor/bundle.esbuild.ts
+++ packages/app-executor/bundle.esbuild.ts
@@ -21,6 +21,25 @@ const notExternalPlugin: esbuild.Plugin = {
     });
   },
 };
+
+const corePlugin: esbuild.Plugin = {
+  name: 'rivet-core',
+  setup(build) {
+    build.onResolve({ filter: /\.\.\/\.\.\/core/ }, async (args) => {
+      const result = await build.resolve('../core/src/index.ts', {
+        kind: args.kind,
+        resolveDir: '.',
+      });
+
+      if (result.errors.length > 0) {
+        return { errors: result.errors };
+      }
+
+      return { path: result.path, external: false };
+    });
+  },
+};
+
 esbuild.build({
   entryPoints: ['bin/executor.ts'],
   bundle: true,
@@ -31,6 +50,7 @@ esbuild.build({
   external: [],
   plugins: [
     notExternalPlugin,
+    corePlugin,
     copy({
       assets: [
         {
