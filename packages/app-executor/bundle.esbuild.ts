60cb808564761ed37af4f9522263c80d57be4d9e
Andy Brenneke <andy.brenneke@ironcladhq.com>
2023-07-10 11:47:58 -0700

    Revert "Fix app-executor bundle without build having happened"
    This reverts commit ee5a3f62ae856f27f5540a8a96345d4c297a16d4.

--- packages/app-executor/bundle.esbuild.ts
+++ packages/app-executor/bundle.esbuild.ts
@@ -2,36 +2,22 @@ import * as esbuild from 'esbuild';
 import { copy } from 'esbuild-plugin-copy';
 import { dirname, join } from 'node:path';
 
-const resolveRivet: esbuild.Plugin = {
-  name: 'resolve-rivet',
-  setup(build) {
-    build.onResolve({ filter: /rivet/ }, (args) => {
-      const [, pkg] = args.path.split('/');
-      console.dir({ pkg });
-      return {
-        path: '',
-      };
-    });
-  },
-};
-
-// esbuild.build({
-//   entryPoints: ['bin/executor.ts'],
-//   bundle: true,
-//   platform: 'node',
-//   outfile: './bin/executor-bundle.js',
-//   format: 'cjs',
-//   target: 'node16',
-//   external: ['@ironclad/rivet-node'],
-//   plugins: [
-//     resolveRivet,
-//     copy({
-//       assets: [
-//         {
-//           from: join(dirname(require.resolve('@dqbd/tiktoken')), 'tiktoken_bg.wasm'),
-//           to: './tiktoken_bg.wasm',
-//         },
-//       ],
-//     }),
-//   ],
-// });
+esbuild.build({
+  entryPoints: ['bin/executor.ts'],
+  bundle: true,
+  platform: 'node',
+  outfile: './bin/executor-bundle.js',
+  format: 'cjs',
+  target: 'node16',
+  external: [],
+  plugins: [
+    copy({
+      assets: [
+        {
+          from: join(dirname(require.resolve('@dqbd/tiktoken')), 'tiktoken_bg.wasm'),
+          to: './tiktoken_bg.wasm',
+        },
+      ],
+    }),
+  ],
+});
