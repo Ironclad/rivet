847350bd88cd4644320e800001e35338eeeeb725
Andy Brenneke <andy.brenneke@ironcladhq.com>
2023-07-28 10:41:06 -0700

    Fix reconnecting flickering

--- packages/app/src/components/ActionBar.tsx
+++ packages/app/src/components/ActionBar.tsx
@@ -11,10 +11,6 @@ import { ReactComponent as MultiplyIcon } from 'majesticons/line/multiply-line.s
 import { ReactComponent as PauseIcon } from 'majesticons/line/pause-circle-line.svg';
 import { ReactComponent as PlayIcon } from 'majesticons/line/play-circle-line.svg';
 import { ReactComponent as MoreMenuVerticalIcon } from 'majesticons/line/more-menu-vertical-line.svg';
-import { ReactComponent as LinkIcon } from 'majesticons/line/link-circle-line.svg';
-import { ReactComponent as GearIcon } from 'majesticons/line/settings-cog-line.svg';
-import { ReactComponent as CpuIcon } from 'majesticons/line/cpu-line.svg';
-import { ReactComponent as ForwardCircleIcon } from 'majesticons/line/forward-circle-line.svg';
 import Popup from '@atlaskit/popup';
 import { settingsModalOpenState } from './SettingsModal';
 import { useRemoteDebugger } from '../hooks/useRemoteDebugger';
@@ -22,6 +18,7 @@ import { isInTauri } from '../utils/tauri';
 import Select from '@atlaskit/select';
 import Portal from '@atlaskit/portal';
 import { debuggerPanelOpenState } from '../state/ui';
+import { ActionBarMoreMenu } from './ActionBarMoreMenu';
 
 const styles = css`
   position: fixed;
@@ -154,7 +151,7 @@ export const ActionBar: FC<ActionBarProps> = ({
 
   return (
     <div css={styles}>
-      {isActuallyRemoteDebugging && (
+      {(isActuallyRemoteDebugging || remoteDebugger.reconnecting) && (
         <div
           className={clsx('remote-debugger-button active', {
             reconnecting: remoteDebugger.reconnecting,
@@ -216,7 +213,7 @@ export const ActionBar: FC<ActionBarProps> = ({
       <Popup
         isOpen={menuIsOpen}
         onClose={() => setMenuIsOpen(false)}
-        content={() => <MoreMenu onClose={() => setMenuIsOpen(false)} />}
+        content={() => <ActionBarMoreMenu onClose={() => setMenuIsOpen(false)} />}
         placement="bottom-end"
         trigger={(triggerProps) => (
           <button className="more-menu" {...triggerProps} onClick={() => setMenuIsOpen(!menuIsOpen)}>
@@ -224,132 +221,6 @@ export const ActionBar: FC<ActionBarProps> = ({
           </button>
         )}
       />
-    </div>
-  );
-};
-
-const moreMenuStyles = css`
-  background-color: var(--grey-darkish);
-  border-radius: 4px;
-  border: 1px solid var(--grey-dark);
-  box-shadow: 3px 1px 10px rgba(0, 0, 0, 0.5);
-  min-width: 250px;
-  display: flex;
-  flex-direction: column;
-
-  * {
-    font-family: 'Roboto', sans-serif;
-  }
-
-  .menu-item-button {
-    padding: 0.5rem 1rem;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-    margin: 0;
-    height: 48px;
-    border-radius: 5px;
-    background-color: transparent;
-    border: none;
-    font-size: 14px;
-    color: var(--grey-lighter);
-
-    &:hover {
-      background-color: rgba(255, 255, 255, 0.1);
-    }
-  }
-
-  .executor {
-    display: flex;
-    align-items: center;
-    padding: 0 1rem;
-    height: 48px;
-
-    .executor-title,
-    .select-executor-remote {
-      color: var(--grey-lighter);
-      font-size: 14px;
-      display: flex;
-      align-items: center;
-      gap: 0.5rem;
-    }
-
-    .select-executor-remote {
-      margin-left: 0.5rem;
-    }
-  }
-`;
-
-const executorOptions = isInTauri()
-  ? ([
-      { label: 'Browser', value: 'browser' },
-      { label: 'Node', value: 'node' },
-    ] as const)
-  : ([{ label: 'Browser', value: 'browser' }] as const);
-
-const MoreMenu: FC<{
-  onClose: () => void;
-}> = ({ onClose }) => {
-  const dropdownTarget = useRef<HTMLDivElement>(null);
-  const setSettingsOpen = useSetRecoilState(settingsModalOpenState);
-  const setDebuggerPanelOpen = useSetRecoilState(debuggerPanelOpenState);
-  const [selectedExecutor, setSelectedExecutor] = useRecoilState(selectedExecutorState);
-  const selectedExecutorOption = executorOptions.find((option) => option.value === selectedExecutor);
-  const { loadRecording } = useLoadRecording();
-
-  const openDebuggerPanel = () => {
-    setDebuggerPanelOpen(true);
-    onClose();
-  };
-
-  const doLoadRecording = () => {
-    loadRecording();
-    onClose();
-  };
-
-  const openSettings = () => {
-    setSettingsOpen(true);
-    onClose();
-  };
-
-  const { remoteDebuggerState: remoteDebugger } = useRemoteDebugger();
-  const isActuallyRemoteDebugging = remoteDebugger.started && !remoteDebugger.isInternalExecutor;
-
-  return (
-    <div css={moreMenuStyles}>
-      <Portal zIndex={1000}>
-        <div ref={dropdownTarget} />
-      </Portal>
-
-      <div className="menu-item executor">
-        <label htmlFor="select-executor" className="executor-title">
-          <CpuIcon /> Executor:
-        </label>
-        {isActuallyRemoteDebugging ? (
-          <span className="select-executor-remote">Remote</span>
-        ) : (
-          <Select
-            id="select-executor"
-            appearance="subtle"
-            options={executorOptions}
-            value={selectedExecutorOption}
-            onChange={(selected) => setSelectedExecutor(selected!.value)}
-            isSearchable={false}
-            isClearable={false}
-            menuPortalTarget={dropdownTarget.current}
-          />
-        )}
-      </div>
-      <div className="menu-item menu-item-button remote-debugger" onClick={openDebuggerPanel}>
-        <LinkIcon /> Remote Debugger
-      </div>
-      <div className="menu-item menu-item-button load-recording" onClick={doLoadRecording}>
-        <ForwardCircleIcon /> Load Recording
-      </div>
-      <div className="menu-item menu-item-button settings" onClick={openSettings}>
-        <GearIcon /> Settings
-      </div>
     </div>
   );
 };
---
+++ packages/app/src/components/ActionBarMoreMenu.tsx
@@ -0,0 +1,141 @@
+import Portal from '@atlaskit/portal';
+import Select from '@atlaskit/select';
+import { css } from '@emotion/react';
+import { FC, useRef } from 'react';
+import { useSetRecoilState, useRecoilState } from 'recoil';
+import { useLoadRecording } from '../hooks/useLoadRecording';
+import { useRemoteDebugger } from '../hooks/useRemoteDebugger';
+import { selectedExecutorState } from '../state/execution';
+import { debuggerPanelOpenState } from '../state/ui';
+import { isInTauri } from '../utils/tauri';
+import { settingsModalOpenState } from './SettingsModal';
+import { ReactComponent as LinkIcon } from 'majesticons/line/link-circle-line.svg';
+import { ReactComponent as GearIcon } from 'majesticons/line/settings-cog-line.svg';
+import { ReactComponent as CpuIcon } from 'majesticons/line/cpu-line.svg';
+import { ReactComponent as ForwardCircleIcon } from 'majesticons/line/forward-circle-line.svg';
+
+const moreMenuStyles = css`
+  background-color: var(--grey-darkish);
+  border-radius: 4px;
+  border: 1px solid var(--grey-dark);
+  box-shadow: 3px 1px 10px rgba(0, 0, 0, 0.5);
+  min-width: 250px;
+  display: flex;
+  flex-direction: column;
+
+  * {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .menu-item-button {
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    margin: 0;
+    height: 48px;
+    border-radius: 5px;
+    background-color: transparent;
+    border: none;
+    font-size: 14px;
+    color: var(--grey-lighter);
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.1);
+    }
+  }
+
+  .executor {
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 48px;
+
+    .executor-title,
+    .select-executor-remote {
+      color: var(--grey-lighter);
+      font-size: 14px;
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+    }
+
+    .select-executor-remote {
+      margin-left: 0.5rem;
+    }
+  }
+`;
+
+const executorOptions = isInTauri()
+  ? ([
+      { label: 'Browser', value: 'browser' },
+      { label: 'Node', value: 'node' },
+    ] as const)
+  : ([{ label: 'Browser', value: 'browser' }] as const);
+
+export const ActionBarMoreMenu: FC<{
+  onClose: () => void;
+}> = ({ onClose }) => {
+  const dropdownTarget = useRef<HTMLDivElement>(null);
+  const setSettingsOpen = useSetRecoilState(settingsModalOpenState);
+  const setDebuggerPanelOpen = useSetRecoilState(debuggerPanelOpenState);
+  const [selectedExecutor, setSelectedExecutor] = useRecoilState(selectedExecutorState);
+  const selectedExecutorOption = executorOptions.find((option) => option.value === selectedExecutor);
+  const { loadRecording } = useLoadRecording();
+
+  const openDebuggerPanel = () => {
+    setDebuggerPanelOpen(true);
+    onClose();
+  };
+
+  const doLoadRecording = () => {
+    loadRecording();
+    onClose();
+  };
+
+  const openSettings = () => {
+    setSettingsOpen(true);
+    onClose();
+  };
+
+  const { remoteDebuggerState: remoteDebugger } = useRemoteDebugger();
+  const isActuallyRemoteDebugging = remoteDebugger.started && !remoteDebugger.isInternalExecutor;
+
+  return (
+    <div css={moreMenuStyles}>
+      <Portal zIndex={1000}>
+        <div ref={dropdownTarget} />
+      </Portal>
+
+      <div className="menu-item executor">
+        <label htmlFor="select-executor" className="executor-title">
+          <CpuIcon /> Executor:
+        </label>
+        {isActuallyRemoteDebugging ? (
+          <span className="select-executor-remote">Remote</span>
+        ) : (
+          <Select
+            id="select-executor"
+            appearance="subtle"
+            options={executorOptions}
+            value={selectedExecutorOption}
+            onChange={(selected) => setSelectedExecutor(selected!.value)}
+            isSearchable={false}
+            isClearable={false}
+            menuPortalTarget={dropdownTarget.current}
+          />
+        )}
+      </div>
+      <div className="menu-item menu-item-button remote-debugger" onClick={openDebuggerPanel}>
+        <LinkIcon /> Remote Debugger
+      </div>
+      <div className="menu-item menu-item-button load-recording" onClick={doLoadRecording}>
+        <ForwardCircleIcon /> Load Recording
+      </div>
+      <div className="menu-item menu-item-button settings" onClick={openSettings}>
+        <GearIcon /> Settings
+      </div>
+    </div>
+  );
+};
