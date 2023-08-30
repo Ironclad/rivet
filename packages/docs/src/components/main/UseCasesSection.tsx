67384311c8bae01136799b136409529464da5450
Cai GoGwilt <cai@ironcladhq.com>
2023-08-30 12:15:09 -0700

    Updates quotes a bit

--- packages/docs/src/components/main/UseCasesSection.tsx
+++ packages/docs/src/components/main/UseCasesSection.tsx
@@ -11,31 +11,25 @@ export const UseCasesSection: React.FC<{ id?: string }> = ({ id }) => {
       <div className={layout.cardGrid}>
         <div className={layout.card3}>
           <p className={styles.quote}>
-            Rivet's visual programming environment feels like a game-changer. You have to experience it to believe it.
+            Rivet's visual programming environment is a game-changer. The visual nature of the tool, paired with how collaborative it is, allows us to create complex chains for AI agents in drastically less time than it would take in other environments. It's the best tool out there.
           </p>
-          <p className={styles.quoteAttribution}>Todd Berman, CTO at Attentive</p>
+          <p className={styles.quoteAttribution}>Todd Berman, CTO at <a href="https://www.attentive.com/" target="_blank">Attentive</a></p>
         </div>
         <div className={layout.card3}>
           <p className={styles.quote}>
-            Rivet is awesome! We're loving Rivet, and figuring out when we can put it out into the wild.
+            Rivet is awesome! It makes it super simple to see what the AI is doing. We've used it to launch our virtual mortgage servicing agent.
           </p>
-          <p className={styles.quoteAttribution}>Teddy Coleman, CTO at Willow Servicing</p>
+          <p className={styles.quoteAttribution}>Teddy Coleman, CTO at <a href="https://www.willowservicing.com/" target="_blank">Willow Servicing</a></p>
         </div>
         <div className={layout.card3}>
           <p className={styles.quote}>Rivet made it super easy to prototype our Bento AI builder experience.</p>
-          <p className={styles.quoteAttribution}>Emily Wang, CEO at Bento</p>
+          <p className={styles.quoteAttribution}>Emily Wang, CEO at <a href="https://www.trybento.co/" target="_blank">Bento</a></p>
         </div>
         <div className={layout.card3}>
           <p className={styles.quote}>
-            We're excited to see Rivet making audio prompt chains easy to prototype and deploy!
+            With AssemblyAI, Rivet makes audio prompt chains easy to prototype and deploy. Supporting sophisticated LLM applications becomes much easier, by decreasing complexity and increasing observability.
           </p>
-          <p className={styles.quoteAttribution}>Domenic Donato, VP of Technology at AssemblyAI</p>
-        </div>
-        <div className={layout.card3}>
-          <p className={styles.quote}>
-            We've had fun experimenting with Rivet, and are excited to natively integrate it with Braintrust!
-          </p>
-          <p className={styles.quoteAttribution}>Ankur Goyal, CEO at Braintrust</p>
+          <p className={styles.quoteAttribution}>Domenic Donato, VP of Technology at <a href="https://www.assemblyai.com/" target="_blank">AssemblyAI</a></p>
         </div>
       </div>
     </Section>
