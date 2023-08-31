99f8b7e1f806ab7e03b9713d8f45a0badd164fb3
Terence Chong <terence.chong@ironcladhq.com>
2023-08-31 14:41:21 -0700

    Remove linux attribution when platform is not linux

--- packages/docs/src/components/main/Footer.tsx
+++ packages/docs/src/components/main/Footer.tsx
@@ -2,14 +2,19 @@ import * as React from 'react';
 
 import layout from '../../css/layout.module.css';
 import styles from './Footer.module.css';
+import { useDownloadUrl } from '../../hooks/useDownloadUrl';
 
 export const Footer: React.FC = () => {
+  const { platform } = useDownloadUrl();
+
   return (
     <footer className={styles.footer}>
       <div className={layout.contentWrapper}>
         <p className={styles.text}>&copy; 2023 Ironclad, Inc. All rights reserved.</p>
         <ul className={styles.attribution}>
-          <li>Linux&#174; is the registered trademark of Linus Torvalds in the U.S. and other countries.</li>
+          {platform === 'linux' && (
+            <li>Linux&#174; is the registered trademark of Linus Torvalds in the U.S. and other countries.</li>
+          )}
         </ul>
       </div>
     </footer>
