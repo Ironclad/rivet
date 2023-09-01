a2e58ccc13869a5930cefaba5588aa5d180e8678
Cai GoGwilt <cai@ironcladhq.com>
2023-09-01 08:27:21 -0700

    Fully approved quotes

--- packages/docs/src/components/main/UseCasesSection.tsx
+++ packages/docs/src/components/main/UseCasesSection.tsx
@@ -17,17 +17,19 @@ export const UseCasesSection: React.FC<{ id?: string }> = ({ id }) => {
         </div>
         <div className={layout.card3}>
           <p className={styles.quote}>
-            Rivet is awesome! It makes it super simple to see what the AI is doing. We've used it to launch our virtual mortgage servicing agent.
+            Rivet really addressed some limitations that we were hitting up against... and some we didn't know we had. The visualization makes a big difference when working through agentic logic and really makes it easy to see what the AI is doing. But the ability to debug and collaborate across the team made a huge difference as well - we've used it to launch our virtual mortgage servicing agent and are excited to see how the tool continues to evolve.
           </p>
           <p className={styles.quoteAttribution}>Teddy Coleman, CTO at <a href="https://www.willowservicing.com/" target="_blank">Willow Servicing</a></p>
         </div>
         <div className={layout.card3}>
-          <p className={styles.quote}>Rivet made it super easy to prototype our Bento AI builder experience.</p>
+          <p className={styles.quote}>
+            In order to build great product experiences we have to be able to iterate quickly. Leveraging tools like Rivet allows us to more accurately understand the tradeoffs between things like speed and quality as we build AI-powered experiences in Bento.
+          </p>
           <p className={styles.quoteAttribution}>Emily Wang, CEO at <a href="https://www.trybento.co/" target="_blank">Bento</a></p>
         </div>
         <div className={layout.card3}>
           <p className={styles.quote}>
-            With AssemblyAI, Rivet makes audio prompt chains easy to prototype and deploy. Supporting sophisticated LLM applications becomes much easier, by decreasing complexity and increasing observability.
+            Rivet is an amazing tool for rapidly prototyping and visually understanding complex AI workflows. It's been wonderful collaborating with Ironclad to integrate AssemblyAI's audio transcription and understanding models into the Rivet ecosystem. We're excited to see what developers create equipped with such a powerful and capable toolkit!
           </p>
           <p className={styles.quoteAttribution}>Domenic Donato, VP of Technology at <a href="https://www.assemblyai.com/" target="_blank">AssemblyAI</a></p>
         </div>
