52c7a559cd5965bcc13ea313c2b74c0f364b7812
Andy Brenneke <andy.brenneke@ironcladhq.com>
2023-09-05 15:13:38 -0700

    Re-enable the autoevals plugin

--- packages/core/src/plugins.ts
+++ packages/core/src/plugins.ts
@@ -1,12 +1,11 @@
 import anthropicPlugin from './plugins/anthropic/index.js';
-// import autoevalsPlugin from './plugins/autoevals/index.js';
+import autoevalsPlugin from './plugins/autoevals/index.js';
 import assemblyAiPlugin from './plugins/assemblyAi/index.js';
 
-// export { anthropicPlugin, autoevalsPlugin, assemblyAiPlugin };
-export { anthropicPlugin, assemblyAiPlugin };
+export { anthropicPlugin, autoevalsPlugin, assemblyAiPlugin };
 
 export const plugins = {
   anthropic: anthropicPlugin,
-  // autoevals: autoevalsPlugin,
+  autoevals: autoevalsPlugin,
   assemblyAi: assemblyAiPlugin,
 };
