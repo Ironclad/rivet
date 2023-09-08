fc9fbb4ec1c6b3e01fd2930dafca7adc99f88d2d
Terence Chong <terence.chong@ironcladhq.com>
2023-09-08 10:53:41 -0700

    Clean up styling

--- packages/docs/src/components/main/DemoVideoSection.tsx
+++ packages/docs/src/components/main/DemoVideoSection.tsx
@@ -8,16 +8,15 @@ export const DemoVideoSection: React.FC<{ id?: string }> = ({ id }) => {
     <Section id={id}>
       <h2>See it in Action</h2>
       <div className={styles.videoContainer}>
-      <iframe
-        className={styles.video}
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/HhgM9MiShwA?si=ULy6PThLVtV7D-Ab&rel=0"
-        title="Rivet Demo"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      />
+        <iframe
+          className={styles.video}
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/HhgM9MiShwA?si=ULy6PThLVtV7D-Ab&rel=0"
+          title="Rivet Demo"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
       </div>
     </Section>
   );
--- packages/docs/src/components/main/FeaturesSection.tsx
+++ packages/docs/src/components/main/FeaturesSection.tsx
@@ -7,16 +7,16 @@ export const FeaturesSection: React.FC<{ id?: string }> = ({ id }) => {
   return (
     <Section id={id}>
       <h2>Why Rivet?</h2>
-      <div className={layout.cardGrid}>
-        <div className={layout.card3}>
+      <div className={layout.card3}>
+        <div>
           <h3>Visualize and Build</h3>
           <p>Visualize and build complex chains to create applications for production — not just prototyping.</p>
         </div>
-        <div className={layout.card3}>
+        <div>
           <h3>Debug Remotely</h3>
           <p>See what's under the hood and observe the execution of prompt chains in your application, in real-time.</p>
         </div>
-        <div className={layout.card3}>
+        <div>
           <h3>Collaborate</h3>
           <p>
             Rivet graphs are just YAML files, so you can version them in your team's repository, and review them using
