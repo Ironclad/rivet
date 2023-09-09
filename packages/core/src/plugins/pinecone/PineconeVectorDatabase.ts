aa2a368c5b478cba70f70bdb27d50962392588b7
Michael Kuc <michaelkuc6@gmail.com>
2023-09-09 21:43:09 +0100

    Use crypto-js for pinecone ID calculation.
    Avoid using the subtle crypto library, to avoid bumping the node dependency
    version. This uses a pure JS implementation via crypto-js.

--- packages/core/src/plugins/pinecone/PineconeVectorDatabase.ts
+++ packages/core/src/plugins/pinecone/PineconeVectorDatabase.ts
@@ -7,21 +7,7 @@ import {
   VectorDatabase,
   coerceType,
 } from '@ironclad/rivet-core';
-
-async function sha256(message: string): Promise<string> {
-  // encode as UTF-8
-  const msgBuffer = new TextEncoder().encode(message);
-
-  // hash the message
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-
-  // convert ArrayBuffer to Array
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-
-  // convert bytes to hex string
-  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
-  return hashHex;
-}
+import * as CryptoJS from 'crypto-js';
 
 export class PineconeVectorDatabase implements VectorDatabase {
   #apiKey;
@@ -34,7 +20,7 @@ export class PineconeVectorDatabase implements VectorDatabase {
     const collectionDetails = getCollection(coerceType(collection, 'string'));
 
     if (!id) {
-      id = await sha256(vector.value.join(','));
+      id = CryptoJS.SHA256(vector.value.join(',')).toString(CryptoJS.enc.Hex);
     }
 
     const response = await fetch(`${collectionDetails.host}/vectors/upsert`, {
