1c3b659a92102eb75dfc8caed39f08ca4f136c54
Andy Brenneke <andy.brenneke@ironcladhq.com>
2023-10-10 12:27:01 -0700

    Fix app not working

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -11,7 +11,7 @@ import { splitVendorChunkPlugin } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    exclude: ['@ironclad/rivet-core', '@ironclad/trivet', 'gpt-tokenizer'],
+    exclude: ['@ironclad/rivet-core', '@ironclad/trivet'],
   },
   resolve: {
     preserveSymlinks: true,
