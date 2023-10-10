4b2b4638c66d7e7348a0f443fdc77f1eb864685c
Andy Brenneke <andy.brenneke@ironcladhq.com>
2023-10-10 15:41:11 -0700

    External Call docs

--- packages/docs/sidebars.js
+++ packages/docs/sidebars.js
@@ -59,7 +59,7 @@ const sidebars = {
         'user-guide/plugins/built-in/autoevals',
         'user-guide/plugins/built-in/gentrace',
         'user-guide/plugins/built-in/huggingface',
-        'user-guide/plugins/built-in/pinecone'
+        'user-guide/plugins/built-in/pinecone',
       ],
     },
     {
