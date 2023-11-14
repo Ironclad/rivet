ee5939578e13825991b534f11b16fc0ddba75318
Andy Brenneke <andy.brenneke@ironcladhq.com>
2023-11-14 13:48:07 -0800

    Fix extra console.log

--- packages/app/src/hooks/useLoadProjectWithFileBrowser.ts
+++ packages/app/src/hooks/useLoadProjectWithFileBrowser.ts
@@ -22,24 +22,22 @@ export function useLoadProjectWithFileBrowser() {
       await ioProvider.loadProjectData(({ project, testData, path }) => {
         const { data, ...projectData } = project;
 
-        console.log(path, project.metadata.id);
-
-        if (
-          Object.values(openedProjects).some((p) => p.fsPath === path)
-        ) {
-          toast.error(
-            `That project is already open.`,
-          );
+        if (Object.values(openedProjects).some((p) => p.fsPath === path)) {
+          toast.error(`That project is already open.`);
           return;
         }
 
-        const alreadyOpenedProject = Object.values(openedProjects).find((p) => p.project.metadata.id === project.metadata.id);
+        const alreadyOpenedProject = Object.values(openedProjects).find(
+          (p) => p.project.metadata.id === project.metadata.id,
+        );
 
-        if (
-          alreadyOpenedProject
-        ) {
+        if (alreadyOpenedProject) {
           toast.error(
-            `"${alreadyOpenedProject.project.metadata.title} [${alreadyOpenedProject.fsPath.split('/').pop()}]" shares the same ID (${project.metadata.id}) and is already open. Please close that project first to open this one.`,
+            `"${alreadyOpenedProject.project.metadata.title} [${alreadyOpenedProject.fsPath
+              .split('/')
+              .pop()}]" shares the same ID (${
+              project.metadata.id
+            }) and is already open. Please close that project first to open this one.`,
           );
           return;
         }
