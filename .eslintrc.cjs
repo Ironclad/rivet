00d357bb01e4f91bc985637bdad750987ae0fcbd
Zhang Yi Jiang <mediumdeviation@gmail.com>
2024-12-11 13:50:15 -0800

    Fix top level lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
   },
   root: true,
-  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  extends: ['standard-with-typescript'],
   plugins: ['import', '@typescript-eslint'],
   overrides: [
     {
