113d978fb2969dcd321ffce2aec79dbb802411e5
Andy Brenneke <andy.brenneke@ironcladhq.com>
2025-02-12 15:54:02 -0800

    CLI fixes

--- packages/cli/bin/cli.js
+++
@@ -1,9 +0,0 @@
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import { makeCommand as makeRunCommand, run } from './commands/run.js';
-import { makeCommand as makeServeCommand, serve } from './commands/serve.js';
-await yargs(hideBin(process.argv))
-    .command('run <projectFile> [graphName]', 'Run a graph in a project file, or the main graph if graphName is not specified.', (y) => makeRunCommand(y), (args) => run(args))
-    .command('serve [projectFile]', 'Serve a project file as a REST API.', (y) => makeServeCommand(y), (args) => serve(args))
-    .demandCommand()
-    .parseAsync();
--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
