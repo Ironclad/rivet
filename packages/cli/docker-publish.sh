6452ec0c74cd7211647a6a6da36300a0aae3a550
Andy Brenneke <andy.brenneke@ironcladhq.com>
2025-02-17 12:39:10 -0800

    Fix amd64/arm64 docker stuff

--- packages/cli/docker-publish.sh
+++ packages/cli/docker-publish.sh
@@ -6,7 +6,10 @@ VERSION=$(node -p "require('./package.json').version")
 
 echo "Publishing version $VERSION..."
 
-docker build -t abrennekeironclad/rivet-server:$VERSION -t abrennekeironclad/rivet-server:latest .
+docker build -t abrennekeironclad/rivet-server:$VERSION -t abrennekeironclad/rivet-server:latest . --platform=linux/amd64
+docker build -t abrennekeironclad/rivet-server:$VERSION-arm64 -t abrennekeironclad/rivet-server:latest-arm64 . --platform=linux/arm64
 
 docker push abrennekeironclad/rivet-server:$VERSION
+docker push abrennekeironclad/rivet-server:$VERSION-arm64
 docker push abrennekeironclad/rivet-server:latest
+docker push abrennekeironclad/rivet-server:latest-arm64
