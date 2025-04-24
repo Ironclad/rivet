c41cc28712f987de979d088701a12287d9146a56
Aryaman Agrawal <aryaman.agrawal@ironcladhq.com>
2025-04-23 20:07:35 -0700

    remove mcp from core

--- .pnp.cjs
+++ .pnp.cjs
@@ -7060,7 +7060,6 @@ const RAW_RUNTIME_STATE =
           ["@google-cloud/vertexai", "npm:0.1.3"],\
           ["@google/generative-ai", "npm:0.24.0"],\
           ["@huggingface/inference", "npm:2.6.4"],\
-          ["@modelcontextprotocol/sdk", "npm:1.10.2"],\
           ["@swc/core", "virtual:36b10b38a2f3e49d8cd162fbeda8e49e065ae2d0ebbbd25b0a3339d0203e0ec0394d852d27989cf9f4297d5cf181367ebbc4ea51690fe53337f9c0c2783178a4#npm:1.3.94"],\
           ["@types/crypto-js", "npm:4.1.3"],\
           ["@types/eslint", "npm:8.44.6"],\
