a9ecc062932b379f663f7551a4bed91f74f90739
Aryaman Agrawal <aryaman.agrawal@ironcladhq.com>
2025-05-20 00:51:34 +0530

    update template

--- packages/app/src/hooks/useNewProjectFromTemplate.ts
+++ packages/app/src/hooks/useNewProjectFromTemplate.ts
@@ -35,13 +35,18 @@ export function useNewProjectFromTemplate() {
         oldNewGraphIdMapping[graph.metadata!.id!] = duplicated.metadata!.id!;
       }
 
-      // Subgraph node is the only node that maintains a reference to another graph,
-      // so we need to update the graphId for all subgraph nodes
+      // Subgraph node and Loop Until node are the only nodes that maintain a reference to another graph,
+      // so we need to update the graphId for them
       for (const graph of newGraphs) {
         for (const node of graph.nodes) {
           const builtInNode = node as BuiltInNodes;
           if (builtInNode.type === 'subGraph') {
             builtInNode.data.graphId = oldNewGraphIdMapping[builtInNode.data.graphId]!;
+          }
+          if (builtInNode.type === 'loopUntil') {
+            builtInNode.data.targetGraph = builtInNode.data.targetGraph
+              ? oldNewGraphIdMapping[builtInNode.data.targetGraph]
+              : undefined;
           }
         }
       }
